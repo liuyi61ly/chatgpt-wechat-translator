d567e00e6bf916d36775c33ca5da722c9eb3cdd3
liuyi61ly <liuyi61ly@hotmail.com>
2023-03-31 16:56:43 +0800

    Update message.js

--- src/message.js
+++ src/message.js
@@ -43,7 +43,7 @@ export async function processMessage(msg) {
 
         console.log(`group content: ${text}`);
         if (text) {
-            text = “请进行中英文翻译：”+text.trim();
+            text = "请进行中英文翻译："+text.trim();
             const reply = await getChatGPTReply(text)
             await room.say(reply);
         }
